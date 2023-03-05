d85592ec26e18e1a988dd6603174eba6c18ad608
Rijk van Putten <rijk@rijkvp.nl>
2023-03-05 14:46:25 +0000

    Fix error messages in querier

--- public/mysql-querier/php/run.php
+++ public/mysql-querier/php/run.php
@@ -24,41 +24,54 @@ if (!$dbConn->query("USE " . $input->db)) {
 
 header("Content-Type: application/json");
 
-if (!$result = $dbConn->query($input->query)) {
+try {
+    if (!$result = $dbConn->query($input->query)) {
+        $error = array();
+        $error["success"] = false;
+        $error["hasData"] = false;
+        $error["error"] = $dbConn->error;
+        echo json_encode($error);
+    } else {
+        $output = (object)[];
+        if (is_bool($result)) {
+            $output->success = $result;
+        } else {
+            // Return JSON columns
+            $columnNames = [];
+            foreach (mysqli_fetch_fields($result) as $field) {
+                $columnNames[] = $field->name;
+            }
+            $pageSize = (int) $input->pageSize;
+            $page = (int) $input->page;
+            $count = $result->num_rows;
+            $pageCount = (int) ceil($count / $pageSize);
+            $from = $page * $pageSize;
+            $to = min($page * $pageSize + $pageSize, $count);
+
+            $rows = [];
+            $counter = 0;
+            while ($row = $result->fetch_row()) {
+                if ($counter >= $from && $counter <= $to) {
+                    $rows[] = array_values($row);
+                }
+                $counter++;
+            }
+
+            $output->success = true;
+            $output->hasData = true;
+            $output->count = $count;
+            $output->pageCount = $pageCount;
+            $output->from = $from;
+            $output->to = $to;
+            $output->headers = $columnNames;
+            $output->data = $rows;
+        }
+        echo json_encode($output);
+    }
+} catch (Exception $e) {
     $error = array();
     $error["success"] = false;
     $error["hasData"] = false;
-    $error["error"] = $dbConn->error;
+    $error["error"] = $e->getMessage();
     echo json_encode($error);
-} else {
-    $columnNames = [];
-    foreach (mysqli_fetch_fields($result) as $field) {
-        $columnNames[] = $field->name;
-    }
-    $pageSize = (int) $input->pageSize;
-    $page = (int) $input->page;
-    $count = $result->num_rows;
-    $pageCount = (int) ceil($count / $pageSize);
-    $from = $page * $pageSize;
-    $to = min($page * $pageSize + $pageSize, $count);
-
-    $rows = [];
-    $counter = 0;
-    while ($row = $result->fetch_row()) {
-        if ($counter >= $from && $counter <= $to) {
-            $rows[] = array_values($row);
-        }
-        $counter++;
-    }
-
-    $output = (object)[];
-    $output->success = true;
-    $output->hasData = true;
-    $output->count = $count;
-    $output->pageCount = $pageCount;
-    $output->from = $from;
-    $output->to = $to;
-    $output->headers = $columnNames;
-    $output->data = $rows;
-    echo json_encode($output);
 }
