460020812818c80e04293537e297a37f1fe23a32
Rijk van Putten <rijk@rijkvp.nl>
2023-03-12 15:50:31 +0000

    Fix startup scripts 2

--- setup/import-dbs.sh
+++ setup/import-dbs.sh
@@ -4,6 +4,6 @@ for db_path in ./databases/*.sql;
 do
     db_name=$(basename $db_path .sql)
     echo "Importing '$db_name'.."
-    mysql -e "DROP DATABASE IF EXISTS $db_name; CREATE DATABASE $db_name;"
-    mysql --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;" $db_name < $db_path
+    sudo mysql -e "DROP DATABASE IF EXISTS $db_name; CREATE DATABASE $db_name;"
+    sudo mysql --init-command="SET SESSION FOREIGN_KEY_CHECKS=0;" $db_name < $db_path
 done
