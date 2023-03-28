ef93d9ec0e07c2355b2469689da2d3d434ad2b42
Rijk van Putten <rijk@rijkvp.nl>
2023-03-28 08:39:26 +0000

    Create symlinks on startup

--- setup/start-apache.sh
+++ setup/start-apache.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+# Create symlinks to project directory
+MAIN_DIR="$PWD/public"
+LM_DIR="$PWD/lesmateriaal/public"
+sudo rm -rf /var/www/*
+sudo ln -sf $MAIN_DIR /var/www/main
+sudo ln -sf $LM_DIR /var/www/lesmateriaal/public
+# Start the apache service
 sudo service apache2 start
