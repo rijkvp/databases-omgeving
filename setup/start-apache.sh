590f99df72ac8fdaf5a717aaf13c253969a62161
Rijk van Putten <rijk@rijkvp.nl>
2023-03-28 08:41:00 +0000

    Fix typo

--- setup/start-apache.sh
+++ setup/start-apache.sh
@@ -4,6 +4,6 @@ MAIN_DIR="$PWD/public"
 LM_DIR="$PWD/lesmateriaal/public"
 sudo rm -rf /var/www/*
 sudo ln -sf $MAIN_DIR /var/www/main
-sudo ln -sf $LM_DIR /var/www/lesmateriaal/public
+sudo ln -sf $LM_DIR /var/www/lesmateriaal
 # Start the apache service
 sudo service apache2 start
